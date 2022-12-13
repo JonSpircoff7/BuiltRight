ef149bfdb592dd1958ef681368f2c1969118f71e
sbullocks <sbullocks@gmail.com>
2022-12-13 17:09:07 -0500

    updated title for select a workout

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -45,7 +45,7 @@ function ProductList() {
 
   return (
     <div className="my-2">
-      <h2>Our Products:</h2>
+      <h2>Select A Workout:</h2>
       {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
