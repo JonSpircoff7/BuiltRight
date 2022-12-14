7481519269d7c5b736da904e231754377d7dae2a
sbullocks <sbullocks@gmail.com>
2022-12-14 11:01:14 -0500

    updated PRODUCTS to EXERCISES.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_PRODUCTS = gql`
+export const QUERY_EXERCISES = gql`
   query getProducts($bodypart: ID) {
     products(bodypart: $bodypart) {
       _id
@@ -24,7 +24,7 @@ export const QUERY_CHECKOUT = gql`
   }
 `;
 
-export const QUERY_ALL_PRODUCTS = gql`
+export const QUERY_ALL_EXERCISES = gql`
   {
     products {
       _id
