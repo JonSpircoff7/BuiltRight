5ee2a54e7b3994e90363ec1557c0e97cc5e0b083
Jonathan <Spircoff_j@yahoo.com>
2022-12-14 14:41:57 -0500

    Added readme revision and fetch for data with filter

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,17 +1,41 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import FormData from "form-data";
+import axios from "axios";
+
+var data = new FormData();
+
+var config = {
+  method: "get",
+  url: "https://wger.de/api/v2/exercise/",
+  data: data,
+};
+
+axios
+  .get("https://wger.de/api/v2/exerciseinfo/?limit=1000")
+  .then((response) => {
+    const results = response.data.results.filter((item) => {
+      return (
+        item.videos[0] &&
+        item.language.short_name == "en" &&
+        item.description != []
+      );
+    });
+    // Return only items that meet certain criteria
+    console.log(results);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
+            <Link to="/orderHistory">Order History</Link>
           </li>
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -25,14 +49,10 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       );
@@ -43,14 +63,14 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🏋️</span>
+          <span role="img" aria-label="shopping bag">
+            🏋️
+          </span>
           BuiltRight
         </Link>
       </h1>
 
-      <nav>
-        {showNavigation()}
-      </nav>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
