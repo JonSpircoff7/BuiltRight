2c8d559e07854fbd9582f7038e674b6bd3915a2a
sbullocks <sbullocks@gmail.com>
2022-12-15 10:41:59 -0500

    updated detail.js with instruction from price.

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -92,7 +92,7 @@ function Detail() {
           <p>{currentExercise.description}</p>
 
           <p>
-            <strong>Price:</strong>${currentExercise.price}{' '}
+            <strong>Instruction:</strong>${currentExercise.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
             <button
               disabled={!cart.find((p) => p._id === currentExercise._id)}
