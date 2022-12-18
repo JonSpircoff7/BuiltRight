3acf962c4a33fe621cab06973db0681c7e08fcf0
Jose Plasencia <josejpd3@gmail.com>
2022-12-18 13:28:20 -0500

    import axios in client/src/pages/Detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -14,6 +14,7 @@ import Cart from '../components/Cart';
 // import { idbPromise } from '../utils/helpers';
 // import spinner from '../assets/spinner.gif';
 
+import axios from 'axios';
 function Detail() {
   // const [state, dispatch] = useStoreContext();
   // const { id } = useParams();
